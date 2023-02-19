dbee42d5966a7919fdc0cc4ca6e9bdf7a2ba3d88
dronushka <gworlds@gmail.com>
2023-02-19 14:40:19 +0300

    user roles

---
+++ src/prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+  await prisma.role.createMany({
+    data: [
+        {
+            name: "customer"
+        },
+        {
+            name: "admin"
+        }
+    ]
+  })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
--- src/src/pages/api/createTestUser.ts
+++ src/src/pages/api/createTestUser.ts
@@ -8,7 +8,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           name: 'Alice',
           email: req.query.email as string,
           nickname: "Acya",
-          age: 13
+          age: 13,
+          roleId: 1
         },
       })
       console.log(user)
