e97711d170bdeb269652f2c8b1b28a13e50cf43c
dronushka <gworlds@gmail.com>
2023-03-06 15:41:08 +0300

    main page shows venues

--- src/src/app/client-main.tsx
+++ src/src/app/client-main.tsx
@@ -1,15 +1,21 @@
 "use client"
 import FullPageMessage from "@/components/full-page-message"
 import { Box, Button, Stack, Text } from "@mantine/core"
+import { Venue } from "@prisma/client"
 import Link from "next/link"
 
-export default function ClientMain() {
+export default function ClientMain({ venues }: { venues: (Omit<Venue, "start"> & { start: string })[] }) {
     return <FullPageMessage>
         <Stack>
             <Text>Бла, бла, бла важная информация</Text>
-            <Link href="/orders/make" passHref legacyBehavior>
-                <Button component="a">Купить билеты</Button>
-            </Link>
+            {venues.map(venue => <>
+                <Text fw="bold">{venue.name}</Text>
+                <Text>{venue.description}</Text>
+                <Link href={"/orders/make?venue=" + venue.id} passHref legacyBehavior>
+                    <Button component="a">Купить билеты</Button>
+                </Link>
+            </>)}
+
         </Stack>
     </FullPageMessage>
 }
--- src/src/app/page.tsx
+++ src/src/app/page.tsx
@@ -1,5 +1,18 @@
+import { prisma } from "@/db"
 import ClientMain from "./client-main"
 
-export default function Page() {
-    return <ClientMain />
+export default async function Page() {
+    const venues = await prisma.venue.findMany({
+        where: {
+            active: true,
+            start: {
+                gt: new Date()
+            }
+        }
+    })
+
+    return <ClientMain venues={venues.map(venue => ({
+        ...venue,
+        start: venue.start.toLocaleString('ru-RU')
+    }))}/>
 }
