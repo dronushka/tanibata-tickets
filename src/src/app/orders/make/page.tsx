f56c945b263fb6edac5772db164cad555bf2898b
dronushka <gworlds@gmail.com>
2023-03-07 11:36:53 +0300

    venueId query param validation

--- src/src/app/orders/make/page.tsx
+++ src/src/app/orders/make/page.tsx
@@ -1,10 +1,14 @@
-import { prisma } from "@/db"
-import { User } from "@prisma/client"
+import { notFound } from "next/navigation"
 import { authOptions } from "@/pages/api/auth/[...nextauth]"
 import { getServerSession } from "next-auth/next"
+import { prisma } from "@/db"
+import { User } from "@prisma/client"
 import { z } from "zod"
-import { TicketRow } from "@/components/order-make/use-order"
-import MakeOrder from "@/components/order-make/make-order"
+import { TicketRow } from "@/components/MakeOrder/types"
+import FullAreaLoading from "@/components/full-area-loading"
+import FullPageMessage from "@/components/full-page-message"
+import MakeOrder from "@/components/MakeOrder/MakeOrder"
+
 
 export default async function MakeOrderPage({ searchParams }: { searchParams?: { [key: string]: string | string[] | undefined } }) {
     const session = await getServerSession(authOptions)
@@ -19,14 +23,16 @@ export default async function MakeOrderPage({ searchParams }: { searchParams?: {
         })
 
     const venueIdValidator = z.string().regex(/^\d+$/)
-    const vanueIdValidated = venueIdValidator.parse(searchParams?.venue)
+    const venueIdValidated = venueIdValidator.safeParse(searchParams?.venue)
 
+    if (venueIdValidated.success === false)
+        notFound()
     // if (!searchParams?.venue && Number(searchParams?.venue))
     //     return <p>Мероприятие не найдено</p>
 
     const venue = await prisma.venue.findUnique({
         where: {
-            id: Number(vanueIdValidated)
+            id: Number(venueIdValidated.data)
         },
         include: {
             tickets: {
@@ -41,10 +47,12 @@ export default async function MakeOrderPage({ searchParams }: { searchParams?: {
         }
     })
 
-    // const ticketRows: Map<string, Ticket & { priceRange: PriceRange}> = new Map
+    if (venue === null)
+        notFound()
+        
     const ticketRowMap = new Map<string, TicketRow>()
 
-    if (venue?.tickets)
+    if (venue.tickets)
         for (let ticket of venue.tickets) {
             const rowNumber = ticket.rowNumber ?? "default"
 
@@ -55,10 +63,16 @@ export default async function MakeOrderPage({ searchParams }: { searchParams?: {
         }
 
     return <MakeOrder
-        user={user && {
-            ...user,
-            createdAt: user.createdAt.toLocaleString("ru-RU")
-        }}
+        paymentData={
+            {
+                name: user?.name ?? "",
+                email: user?.email ?? "",
+                age: user?.age ? String(user.age) : "",
+                phone: user?.phone ?? "",
+                nickname: user?.nickname ?? "",
+                social: user?.social ?? "",
+            }
+        }
         venue={venue && {
             ...venue,
             start: venue.start.toLocaleString("ru-RU"),
