20ef6f48604191b6e33e9029f61efda310bd164d
dronushka <gworlds@gmail.com>
2023-03-12 13:38:58 +0300

    Help page

--- src/src/app/help/page.tsx
+++ src/src/app/help/page.tsx
@@ -1,7 +1,9 @@
+import Help from "@/components/Help"
+
 export const metadata = {
     title: [process.env.FEST_TITLE, 'Помощь'].join(" | ")
 }
 
 export default function HelpPage () {
-    return <p>Помощь</p>
+    return <Help />
 }
