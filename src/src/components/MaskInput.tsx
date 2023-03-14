097e250e636194f05655129b3693928d8ad0c416
dronushka <gworlds@gmail.com>
2023-03-14 15:21:35 +0300

    mask input props

--- src/src/components/MaskInput.tsx
+++ src/src/components/MaskInput.tsx
@@ -1,17 +1,21 @@
-import { Input } from "@mantine/core"
+import { Input, TextInputProps } from "@mantine/core"
 import { useId } from "@mantine/hooks"
 import { MaskedInput, createDefaultMaskGenerator } from 'react-hook-mask'
 
 export default function MaskInput (
-    {label, name, maxLength, value, mask, withAsterisk, error, onChange}: 
-    {label?: string, name?: string, maxLength: number, value: string, mask: string, withAsterisk?: boolean, error?: string, onChange?: (value: string) => void  }
+    {mask, disabled, label, name, maxLength, value, withAsterisk, error, onChange}: 
+    Omit<TextInputProps,"value"|"onChange"> & { value: string, onChange: ((value: string) => void) | undefined, mask: string }
+    // {label?: string, name?: string, maxLength: number, value: string, , withAsterisk?: boolean, error?: string, onChange?: (value: string) => void  }
 ) {
     const id = useId()
     const maskGenerator = createDefaultMaskGenerator(mask)
-    // console.log('phone error', error)
+    if (!value || !onChange)
+        return <></>
+        
     return (
         <Input.Wrapper id={id} label={label} withAsterisk={withAsterisk} error={error}>
             <Input
+                disabled={disabled}
                 invalid={!!error}
                 id={id}
                 name={name}
@@ -23,4 +27,6 @@ export default function MaskInput (
             />
         </Input.Wrapper>
     )
+
+
 }
