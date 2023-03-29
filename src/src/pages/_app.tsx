bf2f7177f90faa806b43afa9e3211b3694c9ee5b
dronushka <gworlds@gmail.com>
2023-03-29 19:34:50 +0300

    trpc wip

--- src/src/pages/_app.tsx
+++ src/src/pages/_app.tsx
@@ -1,21 +1,11 @@
 import { useState } from 'react'
+import type { AppType } from 'next/app'
 import NextApp, { AppProps, AppContext } from 'next/app'
-// import { getCookie, setCookie } from 'cookies-next';
 import Head from 'next/head'
-import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core'
-// import { NotificationsProvider } from '@mantine/notifications';
+import { trpc } from '../lib/trpc'
+import { MantineProvider } from '@mantine/core'
 
-export default function App(props: AppProps & { colorScheme: ColorScheme }) {
-  const { Component, pageProps } = props
-  const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme)
-
-  const toggleColorScheme = (value?: ColorScheme) => {
-    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark')
-    setColorScheme(nextColorScheme)
-    // setCookie('mantine-color-scheme', nextColorScheme, { maxAge: 60 * 60 * 24 * 30 });
-  }
-
-  return (
+const App: AppType = ({ Component, pageProps }) => 
     <>
       <Head>
         <title>Нян-Фест 2023</title>
@@ -23,21 +13,10 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
 
-      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-        <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-          {/* <NotificationsProvider> */}
-            <Component {...pageProps} />
-          {/* </NotificationsProvider> */}
-        </MantineProvider>
-      </ColorSchemeProvider>
-    </>
-  )
-}
 
-App.getInitialProps = async (appContext: AppContext) => {
-  const appProps = await NextApp.getInitialProps(appContext)
-  return {
-    ...appProps,
-    // colorScheme: getCookie('mantine-color-scheme', appContext.ctx) || 'dark',
-  }
-}
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+
+export default trpc.withTRPC(App)
