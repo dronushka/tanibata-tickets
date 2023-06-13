eab1286574a4b9f68e53a9feecd469a228d4fbbc
arrdev <gworlds@gmail.com>
2023-06-13 19:50:52 +0000

    cheque upload wip

--- src/src/components/orders/client/OrdersForm.tsx
+++ src/src/components/orders/client/OrdersForm.tsx
@@ -8,23 +8,12 @@ import { useState } from "react"
 import OrderStatusText from "./OrderStatusText"
 import { useRouter } from "next/navigation"
 import OrderStatusTooltip from "./OrderStatusTooltip"
-import MyRpcClientComponent from "@/components/MyRpcClientComponent"
+import OrderItem from "./OrderItem"
 
-type HydratedOrder = Omit<Order, "createdAt"> & (
-    {
-        createdAt: string,
-        venue: Omit<Venue, "start"> & { start: string, priceRange: PriceRange[] } | null
-        cheque: DBFile | null,
-        tickets: (Ticket & (
-            {
-                venue: Omit<Venue, "start"> & { start: string } | null
-                priceRange: PriceRange | null
-            }
-        ))[]
-    }
-)
 
-export default function OrdersForm({ orders }: { orders: HydratedOrder[] }) {
+
+
+export default function OrdersForm({ orders }) {
     
     const router = useRouter()
 
@@ -99,141 +88,8 @@ export default function OrdersForm({ orders }: { orders: HydratedOrder[] }) {
     return (
         <>
             <Stack>
-                <MyRpcClientComponent/>
                 {orders.map(order => (
-                    <Paper
-                        key={order.id}
-                        shadow="md"
-                        p="md"
-                        sx={{
-                            maxWidth: 450
-                        }}
-                    >
-                        <Stack>
-                            <Group>
-                                <Text>Номер заказа: {order.id}</Text>
-                                <Group>
-                                    <Text>Статус:</Text>
-                                    <OrderStatusText status={order.status} />
-                                    <OrderStatusTooltip status={order.status} />
-                                </Group>
-                            </Group>
-                            <Text>{order.createdAt}</Text>
-                            <Text fw="bold">{order.venue?.name}</Text>
-                            {order.isGoodness && <Text
-                                fw="bold"
-                            // sx={{
-                            //     backgroundClip: "text",
-                            //     backgroundImage: "linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red)",
-                            //     color: "transparent"
-                            // }}
-                            >
-                                Добро активировано!
-                            </Text>}
-                            {order.venue?.noSeats === false && <>
-                                <Text>Места:</Text>
-                                <List type="ordered">
-                                    {order.tickets.map(ticket => (
-                                        <List.Item key={ticket.id} >
-                                            <Group>
-                                                {/* <Text>{ticket.venue?.name}</Text> */}
-                                                <Text>Ряд: {ticket.rowNumber} Место: {ticket.number}</Text>
-                                                <Text>{order.isGoodness ? Number(process.env.NEXT_PUBLIC_GOODNESS_PRICE ?? 0).toFixed(2) : ticket.priceRange?.price.toFixed(2)} р.</Text>
-                                            </Group>
-                                        </List.Item>
-                                    ))}
-                                </List>
-                            </>}
-                            {order.venue?.noSeats === true && <>
-                                <Text>Количество билетов: {order.ticketCount}</Text>
-                            </>}
-                            {!!order.comment.length && <>
-                                <Text>Комментарий:</Text>
-                                <Text>{order.comment}</Text>
-                            </>}
-                            <Group>
-                                {
-                                    order.status !== OrderStatus.UNPAID && !order.cheque &&
-                                    <Text color="red">Чек не найден!</Text>
-                                }
-                                {
-                                    order.status !== OrderStatus.UNPAID && order.cheque &&
-                                    <a href={"/api/download/" + order.cheque.id}>Чек приложен</a>
-                                }
-                            </Group>
-                            {(order.status === OrderStatus.UNPAID || order.status === OrderStatus.PENDING) &&
-                                <Group>
-                                    <Box>
-                                        <Text color="gray" size="sm">Файл размером не более 2 Мб,</Text>
-                                        <Text color="gray" size="sm">Файл формате: jpeg, png, pdf</Text>
-                                    </Box>
-                                    <Box>
-                                        {order.id === loading && <Button
-                                            loading
-                                            variant="outline"
-                                            leftIcon={<IconUpload />}
-                                        >
-                                            {order.cheque ? "Приложить другой" : "Приложить чек"}
-                                        </Button>}
-                                        {order.id !== loading && <FileButton
-                                            onChange={(value) => sendCheque(order.id, value)}
-                                            accept="image/png,image/jpeg,application/pdf"
-                                        >
-                                            {(props) => (
-                                                <Button
-                                                    {...props}
-                                                    // loading={order.id === loading}
-                                                    disabled={!!loading && loading !== order.id}
-                                                    color={orderError?.orderId === order.id ? "red" : "primary"}
-                                                    variant="outline"
-                                                    leftIcon={<IconUpload />}
-                                                >
-                                                    {order.cheque ? "Приложить другой" : "Приложить чек"}
-                                                </Button>
-                                            )}
-                                        </FileButton>}
-                                    </Box>
-                                </Group>
-                            }
-                            <Button
-                                leftIcon={<IconDownload />}
-                                disabled={order.status !== OrderStatus.COMPLETE && order.status !== OrderStatus.USED}
-                                component="a"
-                                href={"/api/getTicketsPDF?orderId=" + order.id}
-                            >
-                                Скачать билеты
-                            </Button>
-
-                            {order.status === OrderStatus.UNPAID &&
-                                <Button
-                                    leftIcon={<IconX />}
-                                    loading={loading === order.id}
-                                    disabled={!!loading && loading !== order.id}
-                                    onClick={() => setOrderStatus(order.id)}
-                                >
-                                    Отменить
-                                </Button>
-                            }
-                            {order.status !== OrderStatus.UNPAID &&
-                                <Button
-                                    leftIcon={<IconReceiptRefund />}
-                                    loading={loading === order.id}
-                                    disabled={
-                                        !!loading && loading !== order.id ||
-                                        !order.cheque ||
-                                        order.status === OrderStatus.RETURN_REQUESTED ||
-                                        order.status === OrderStatus.RETURNED ||
-                                        order.status === OrderStatus.USED ||
-                                        order.status === OrderStatus.CANCELLED
-                                    }
-                                    onClick={() => setReturnConfirmationOrderId(order.id)}
-                                >
-                                    Возврат
-                                </Button>
-                            }
-                            <Input.Error>{orderError?.orderId === order.id ? orderError.error : ""}</Input.Error>
-                        </Stack>
-                    </Paper>
+                    <OrderItem key={order.id} order={order} />
                 ))}
 
             </Stack>
--- src/src/lib/trpc/trpc-server.ts
+++ src/src/lib/trpc/trpc-server.ts
@@ -5,29 +5,38 @@ const t = initTRPC.create();
 
 // this is our data store, used to respond to incoming RPCs from the client
 
-interface User {
-    id: string;
-    name: string;
-}
-const userList: User[] = [
-    {
-        id: '1',
-        name: 'KATT',
-    },
-    {
-        id: '2',
-        name: 'Foo',
-    },
-];
+// interface User {
+//     id: string;
+//     name: string;
+// }
+// const userList: User[] = [
+//     {
+//         id: '1',
+//         name: 'KATT',
+//     },
+//     {
+//         id: '2',
+//         name: 'Foo',
+//     },
+// ];
 
 // this is our RPC API
 export const appRouter = t.router({
-    userById: t.procedure
-        .input(z.number())
-        .query((req) => {
-            const { input } = req;
-            return userList.find((u) => parseInt(u.id) === input);
-        }),
+    // userById: t.procedure
+    //     .input(z.number())
+    //     .query((req) => {
+    //         const { input } = req;
+    //         return userList.find((u) => parseInt(u.id) === input);
+    //     }),
+    uploadCheque: t.procedure
+        .input(z.object({
+            orderId: z.number().gt(0, "invalid_order_id"),
+            // cheque: z.custom<File>(val => val instanceof File, "no_file") //TODO validate mime and size
+        }))
+        .mutation(async (opts) => {
+            const { input } = opts;
+            console.log(input)
+        })
 });
 
 export type AppRouter = typeof appRouter;
