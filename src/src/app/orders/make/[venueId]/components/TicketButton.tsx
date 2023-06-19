9a1a6487fb7624b09b65e41ae9866b2f1ddfcd7a
arrdev <gworlds@gmail.com>
2023-06-19 14:11:48 +0000

    optimized disabled ticket buttons

--- src/src/app/orders/make/[venueId]/components/TicketButton.tsx
+++ src/src/app/orders/make/[venueId]/components/TicketButton.tsx
@@ -1,27 +1,24 @@
-import { Button, MantineColor, Popover, Stack, Sx, Text } from "@mantine/core"
+import { Box, Button, MantineColor, Popover, Stack, Sx, Text } from "@mantine/core"
 import { useDisclosure } from "@mantine/hooks"
 import { ClientTicket, ClientTicketSetter } from "./TicketsPicker/TicketsPicker"
 
-export default function TicketButton(
-    {
-        ticket,
-        selected = false,
-        reserved = false,
-        setSelectedTickets,
-        sx
-    }:
-        {
-            ticket: ClientTicket,
-            setSelectedTickets: ClientTicketSetter,
-            selected: boolean,
-            reserved?: boolean,
-            sx: Sx
-        }
-) {
+export default function TicketButton({
+    ticket,
+    selected = false,
+    reserved = false,
+    setSelectedTickets,
+    sx,
+}: {
+    ticket: ClientTicket
+    setSelectedTickets: ClientTicketSetter
+    selected: boolean
+    reserved?: boolean
+    sx: Sx
+}) {
     const [opened, { close, open }] = useDisclosure(false)
 
     const onTicketClick = () => {
-        setSelectedTickets(prev => {
+        setSelectedTickets((prev) => {
             if (prev.has(ticket.id)) {
                 const newTickets = new Map(prev)
                 newTickets.delete(ticket.id)
@@ -29,8 +26,11 @@ export default function TicketButton(
             } else {
                 const newTickets = new Map(prev).set(ticket.id, ticket)
                 return new Map(
-                    [...newTickets.entries()].sort(([k, v], [k2, v2])=> {
-                        return (v.rowNumber ? parseInt(v.rowNumber) : 0) - (v2.rowNumber ? parseInt(v2.rowNumber) : 0) || parseInt(v.number) - parseInt(v2.number)
+                    [...newTickets.entries()].sort(([k, v], [k2, v2]) => {
+                        return (
+                            (v.rowNumber ? parseInt(v.rowNumber) : 0) - (v2.rowNumber ? parseInt(v2.rowNumber) : 0) ||
+                            parseInt(v.number) - parseInt(v2.number)
+                        )
                     })
                 )
             }
@@ -39,40 +39,43 @@ export default function TicketButton(
 
     let colorCode: MantineColor = "green"
 
-    if (selected)
-        colorCode = "#228be6" //"#daca55" //"#da7855"
-    else if (ticket.priceRange?.color)
-        colorCode = ticket.priceRange.color
+    if (selected) colorCode = "#228be6" //"#daca55" //"#da7855"
+    else if (ticket.priceRange?.color) colorCode = ticket.priceRange.color
 
-    return (
-        <Popover width={100} position="bottom" withArrow shadow="md" opened={opened}>
-            <Popover.Target>
-                <Button
-                    disabled={reserved || ticket.reserved}
-                    compact
-                    sx={{
-                        ...sx,
-                        backgroundColor: colorCode,
-                        "&:disabled": {
-                            backgroundColor: "#d4d4d4"
-                        },
-                        "&:hover": {
-                            backgroundColor: "#9775fa"
-                        }
-                    }}
-                    onMouseEnter={open}
-                    onMouseLeave={close}
-                    onClick={onTicketClick}
-                >
-                </Button>
-            </Popover.Target>
-            <Popover.Dropdown sx={{ pointerEvents: 'none' }}>
-                <Stack spacing="xs">
-                    <Text size="xs">Ряд: {ticket.rowNumber}</Text>
-                    <Text size="xs">Место: {ticket.number}</Text>
-                    <Text size="xs">Цена: {ticket.priceRange?.price ?? 0}</Text>
-                </Stack>
-            </Popover.Dropdown>
-        </Popover>
-    )
+    if (reserved || ticket.reserved) return <Box sx={{ 
+        ...sx,
+        backgroundColor: "#d4d4d4",
+        borderRadius: "4px"
+    }} />
+    else
+        return (
+            <Popover width={100} position="bottom" withArrow shadow="md" opened={opened}>
+                <Popover.Target>
+                    <Button
+                        // disabled={reserved || ticket.reserved}
+                        compact
+                        sx={{
+                            ...sx,
+                            backgroundColor: colorCode,
+                            // "&:disabled": {
+                            //     backgroundColor: "#d4d4d4"
+                            // },
+                            "&:hover": {
+                                backgroundColor: "#9775fa",
+                            },
+                        }}
+                        onMouseEnter={open}
+                        onMouseLeave={close}
+                        onClick={onTicketClick}
+                    ></Button>
+                </Popover.Target>
+                <Popover.Dropdown sx={{ pointerEvents: "none" }}>
+                    <Stack spacing="xs">
+                        <Text size="xs">Ряд: {ticket.rowNumber}</Text>
+                        <Text size="xs">Место: {ticket.number}</Text>
+                        <Text size="xs">Цена: {ticket.priceRange?.price ?? 0}</Text>
+                    </Stack>
+                </Popover.Dropdown>
+            </Popover>
+        )
 }
