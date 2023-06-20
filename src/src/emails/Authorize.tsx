dd72c4054797a074398b3a87e44e46a5361e79ad
arrdev <gworlds@gmail.com>
2023-06-20 13:39:11 +0000

    email updates

--- src/src/emails/Authorize.tsx
+++ src/src/emails/Authorize.tsx
@@ -1,14 +1,26 @@
-import * as React from 'react'
-import { Html } from '@react-email/html'
-import EmailSignature from './EmailSignature'
+import * as React from "react"
+import { Html } from "@react-email/html"
+import EmailSignature from "./EmailSignature"
+import { Tailwind } from "@react-email/tailwind"
+import { Body } from "@react-email/body"
 
 export default function Authorize({ url }: { url: string }) {
     return (
         <Html>
-            <p style={{ textAlign: "center" }}>
-                <b>{">>"} <a href={url} target="_blank" rel="noreferrer">Вход в систему покупки билетов</a> {"<<"}</b>
-            </p>
-            <EmailSignature />
+            <Tailwind>
+                <Body className="bg-white">
+                    <p style={{ textAlign: "center" }}>
+                        <b>
+                            {">>"}{" "}
+                            <a href={url} target="_blank" rel="noreferrer">
+                                Вход в систему покупки билетов
+                            </a>{" "}
+                            {"<<"}
+                        </b>
+                    </p>
+                    <EmailSignature />
+                </Body>
+            </Tailwind>
         </Html>
     )
 }
