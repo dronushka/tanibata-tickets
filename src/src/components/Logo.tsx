d7f66ac96e23e2aacbe874aa9efdf7f34b03b5cb
arrdev <gworlds@gmail.com>
2023-06-24 17:02:28 +0000

    new logo

--- src/src/components/Logo.tsx
+++ src/src/components/Logo.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 export default function Logo() {
     return (
         <Link href="/" passHref legacyBehavior>
-            <Box sx={{position: "relative", width: 100, height: 50}}>
+            <Box sx={{position: "relative", width: 180, height: 60}}>
                 <Image src="/logo.png" alt="Нян-Фест 2023 | Главная" fill />
             </Box>
         </Link>
